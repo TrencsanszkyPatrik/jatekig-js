c3c505da5b9ab25b1a82cef88f34992971141005
TrencsanszkyPatrik <90695000+TrencsanszkyPatrik@users.noreply.github.com>
2022-03-09 21:33:44 +0100

    asd

--- jatekmain.js
+++ jatekmain.js
@@ -1,39 +1,41 @@
-
 document.getElementById("user").innerHTML = localStorage.key(0);
 
-document.getElementById("kulcs").innerHTML = sessionStorage.key(3);
-
-
 lomoskulcs = sessionStorage.key(3);
-lomosertek = sessionStorage.getItem(lomoskulcs)
-document.getElementById('kulcs1').innerHTML = lomos
-document.getElementById('kulcs').innerHTML = lomosertek
+lomosertek = sessionStorage.getItem(lomoskulcs);
+document.getElementById("kulcs1").innerHTML = lomoskulcs;
+document.getElementById("kulcs").innerHTML = lomosertek;
 
-
-function el(inv){
-    inv.style.display = "none";
+function el(inv) {
+  inv.style.display = "none";
 }
 
-function inditas(kezdo, lomoss){
-    
-    
-    kezdo.style.display = "none";
-    lomoss.style.display = "block";
+function inditas(kezdo, lomoss) {
+  kezdo.style.display = "none";
+  lomoss.style.display = "block";
 
-    sessionStorage.setItem('A lomos kulcsa', 'Nem rendelkezel vele')
-    sessionStorage.setItem('A mellékhelyiség kulcsa', 'Nem rendelkezel vele')
-    sessionStorage.setItem('A kisszoba kulcsa', 'Nem rendelkezel vele')
-    sessionStorage.setItem('A mellékfolyosó kulcsa', 'Nem rendelkezel vele')
-    sessionStorage.setItem('A nagyszoba kulcsa', 'Nem rendelkezel vele')
-    sessionStorage.setItem('A kijárat kulcsa', 'Nem rendelkezel vele')
-    sessionStorage.setItem('Zseblámpa', 'Rendelkezel vele')
+  sessionStorage.setItem("A lomos kulcsa", "Nem rendelkezel vele");
+  sessionStorage.setItem("A nagyszoba kulcsa", "Nem rendelkezel vele");
+  sessionStorage.setItem("A a kijárat kulcsa", "Nem rendelkezel vele");
+  sessionStorage.setItem("Zseblámpa", "Nem rendelkezel vele");
+}
+function fasz() {
+  alert(sessionStorage.getItem("7"));
 }
 
+function hozzad() {
+  if (sessionStorage.getItem("Zseblámpa") == "Nem rendelkezel vele") {
+    sessionStorage.setItem("Zseblámpa", "Rendelkezel vele (1DB)");
+  } else if (sessionStorage.getItem("Zseblámpa") == "Rendelkezel vele (2DB)") {
+    alert("Kettő");
+  } else {
+    sessionStorage.setItem("Zseblámpa", "Rendelkezel vele (2DB)");
+  }
+}
 
 function eltuntet(x) {
-    if (x.style.display === "block") {
-        x.style.display = "none";
-      } else {
-        x.style.display = "block";
-      }
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
